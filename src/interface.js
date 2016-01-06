5f081699f168c7e2358d9bc3089df6f4d23dd9c9
michaellennox <michaelctlennox@gmail.com>
2016-01-06 16:13:44 +0000

    Adds basic interface interactivity

--- src/interface.js
+++ src/interface.js
@@ -1,7 +1,40 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
+  var $powerSave = $('#power-save');
 
-  $('#temperature').text(thermostat.temp);
+  var updateInfo = function() {
+    $('#temperature').text(thermostat.temp);
+    $('#display').css('background-color', thermostat.displayColour());
+  };
 
+  var togglePowerSaveUI = function() {
+    if ($powerSave.text() === 'PowerSave - On') {
+      $powerSave.text('PowerSave - Off');
+    } else {
+      $powerSave.text('PowerSave - On');
+    }
+  };
 
+  $powerSave.on('click', function() {
+    thermostat.togglePowerSave();
+    togglePowerSaveUI();
+    updateInfo();
+  });
+
+  updateInfo();
+
+  $('#up').on('click', function() {
+    thermostat.up();
+    updateInfo();
+  });
+
+  $('#down').on('click', function() {
+    thermostat.down();
+    updateInfo();
+  });
+
+  $('#reset').on('click', function() {
+    thermostat.resetTemp();
+    updateInfo();
+  });
 });
