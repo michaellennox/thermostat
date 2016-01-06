a236c9f361c34123e9aad4082a915bc0457e961c
michaellennox <michaelctlennox@gmail.com>
2016-01-06 18:10:19 +0000

    Updates jQuery

--- src/interface.js
+++ src/interface.js
@@ -1,40 +1,38 @@
+var thermostat = new Thermostat();
+var $powerSave = $('#power-save');
+
+function updateData() {
+  $('#temperature').text(thermostat.temp);
+  $('#display').css('background-color', thermostat.displayColour());
+}
+
+function togglePowerSaveUI() {
+  if (thermostat.isPowerSaving) {
+    $powerSave.css('background-color', 'green');
+  } else {
+    $powerSave.css('background-color', 'red');
+  }
+}
+
+$(document).on('click', updateData());
+
 $(document).ready(function() {
-  var thermostat = new Thermostat();
-  var $powerSave = $('#power-save');
-
-  var updateInfo = function() {
-    $('#temperature').text(thermostat.temp);
-    $('#display').css('background-color', thermostat.displayColour());
-  };
-
-  var togglePowerSaveUI = function() {
-    if (thermostat.isPowerSaving) {
-      $powerSave.css('background-color', 'green');
-    } else {
-      $powerSave.css('background-color', 'red');
-    }
-  };
+  updateInfo();
 
   $powerSave.on('click', function() {
     thermostat.togglePowerSave();
     togglePowerSaveUI();
-    updateInfo();
   });
-
-  updateInfo();
 
   $('#up').on('click', function() {
     thermostat.up();
-    updateInfo();
   });
 
   $('#down').on('click', function() {
     thermostat.down();
-    updateInfo();
   });
 
   $('#reset').on('click', function() {
     thermostat.resetTemp();
-    updateInfo();
   });
 });
