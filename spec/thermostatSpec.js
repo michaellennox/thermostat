f66a33aaeebff11f7d8ffb97e983f72cd554e9c4
Hamid Zade <hamidmamadzade@gmail.com>
2016-01-06 14:50:53 +0000

    up and down button implemented (and tested)

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,24 +1,29 @@
 describe('Thermostat', function() {
+  var thermostat;
 
-  var thermostat = new Thermostat;
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
   describe('starting temperature for thermostat', function() {
     it('defaults to 20 degrees', function() {
-      expect(thermostat.degrees()).toEqual(20)
+      expect(thermostat.currentTemp()).toEqual(20)
     });
   });
 
-  // describe('', function() {
-  //   it('', function() {
-  //
-  //   });
-  // });
-  //
-  // describe('', function() {
-  //   it('', function() {
-  //
-  //   });
-  // });
+  describe('increase temperature', function() {
+    it('increases temp by 1', function() {
+      thermostat.upButton();
+      expect(thermostat.currentTemp()).toEqual(21);
+    });
+  });
+  
+  describe('decrease temperature', function() {
+    it('decreases temp by 1', function() {
+      thermostat.downButton();
+      expect(thermostat.currentTemp()).toEqual(19);
+    });
+  });
   //
   // describe('', function() {
   //   it('', function() {
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,15 @@
-function Thermostat() {};
+function Thermostat() {
+  this.degrees = 20
+};
 
-Thermostat.prototype.degrees = function (temperature) {
-  return 20;
+Thermostat.prototype.currentTemp = function() {
+  return this.degrees;
+};
+
+Thermostat.prototype.upButton = function() {
+  return this.degrees += 1;
+};
+
+Thermostat.prototype.downButton = function() {
+  return this.degrees -= 1;
 };
