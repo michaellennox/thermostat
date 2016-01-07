ac29a0a70d2ef1d8b37709ffccb4da5198ef6429
michaellennox <michaelctlennox@gmail.com>
2016-01-07 10:39:24 +0000

    Removes more evil tabs

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,90 +1,90 @@
 describe("Thermostat", function() {
-	var thermostat;
+  var thermostat;
 
-	beforeEach(function(){
-		thermostat = new Thermostat();
-	});
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
 
-	it("Initializes with a temp of 20 degrees", function(){
-		expect(thermostat.temp).toEqual(20);
-	});
+  it("Initializes with a temp of 20 degrees", function(){
+    expect(thermostat.temp).toEqual(20);
+  });
 
-	it("Initializes with power saving true", function(){
-		expect(thermostat.isPowerSaving).toBe(true);
-	});
+  it("Initializes with power saving true", function(){
+    expect(thermostat.isPowerSaving).toBe(true);
+  });
 
-	describe("#up", function(){
-		it("Increases the temperature by one", function(){
-			thermostat.up();
-			expect(thermostat.temp).toEqual(21);
-		});
+  describe("#up", function(){
+    it("Increases the temperature by one", function(){
+      thermostat.up();
+      expect(thermostat.temp).toEqual(21);
+    });
 
-		it("When power saving is on, it wont increase past 25", function(){
-			thermostat.temp = 25;
-			expect(function(){thermostat.up();}).toThrow("It's too hot already!");
-		});
+    it("When power saving is on, it wont increase past 25", function(){
+      thermostat.temp = 25;
+      expect(function(){thermostat.up();}).toThrow("It's too hot already!");
+    });
 
-		it("When power saving is off, it wont increase past 32", function(){
-			thermostat.togglePowerSave();
-			thermostat.temp = 32;
-			expect(function(){thermostat.up();}).toThrow("It's too hot already!");
-		});
-	});
+    it("When power saving is off, it wont increase past 32", function(){
+      thermostat.togglePowerSave();
+      thermostat.temp = 32;
+      expect(function(){thermostat.up();}).toThrow("It's too hot already!");
+    });
+  });
 
-	describe("#down", function(){
-		it("Decreases the temp by one", function(){
-			thermostat.down();
-			expect(thermostat.temp).toEqual(19);
-		});
+  describe("#down", function(){
+    it("Decreases the temp by one", function(){
+      thermostat.down();
+      expect(thermostat.temp).toEqual(19);
+    });
 
-		it("Wont decrease past min temp", function(){
-			thermostat.temp = thermostat.minTemp;
-			expect(function(){thermostat.down();}).toThrow("It's wayyyy too cold for that. Go back to Canada");
-		});
-	});
+    it("Wont decrease past min temp", function(){
+      thermostat.temp = thermostat.minTemp;
+      expect(function(){thermostat.down();}).toThrow("It's wayyyy too cold for that. Go back to Canada");
+    });
+  });
 
-	describe("#togglePowerSave", function(){
-		it("turns power saving false when true", function(){
-			thermostat.togglePowerSave();
-			expect(thermostat.isPowerSaving).toBe(false);
-		});
+  describe("#togglePowerSave", function(){
+    it("turns power saving false when true", function(){
+      thermostat.togglePowerSave();
+      expect(thermostat.isPowerSaving).toBe(false);
+    });
 
-		it("turns power saving true when false", function(){
-			thermostat.togglePowerSave();
-			thermostat.togglePowerSave();
-			expect(thermostat.isPowerSaving).toBe(true);
-		});
+    it("turns power saving true when false", function(){
+      thermostat.togglePowerSave();
+      thermostat.togglePowerSave();
+      expect(thermostat.isPowerSaving).toBe(true);
+    });
 
-		it('sets temp to 25 if over 25 when toggling on', function(){
-			thermostat.togglePowerSave();
-			thermostat.temp = 30;
-			thermostat.togglePowerSave();
-			expect(thermostat.temp).toEqual(25);
-		});
-	});
+    it('sets temp to 25 if over 25 when toggling on', function(){
+      thermostat.togglePowerSave();
+      thermostat.temp = 30;
+      thermostat.togglePowerSave();
+      expect(thermostat.temp).toEqual(25);
+    });
+  });
 
-	describe("#resetTemp", function(){
-		it("Resets the temp to 20", function(){
-			thermostat.up();
-			thermostat.resetTemp();
-			expect(thermostat.temp).toEqual(20);
-		});
-	});
+  describe("#resetTemp", function(){
+    it("Resets the temp to 20", function(){
+      thermostat.up();
+      thermostat.resetTemp();
+      expect(thermostat.temp).toEqual(20);
+    });
+  });
 
-	describe("#displayColour", function(){
-		it("Returns red when temp is at or over 25", function(){
-			thermostat.temp = 25;
-			expect(thermostat.displayColour()).toEqual("red");
-		});
+  describe("#displayColour", function(){
+    it("Returns red when temp is at or over 25", function(){
+      thermostat.temp = 25;
+      expect(thermostat.displayColour()).toEqual("red");
+    });
 
-		it("Returns yellow when temp is between 18 to 24", function(){
-			thermostat.temp = 19;
-			expect(thermostat.displayColour()).toEqual("yellow");
-		});
+    it("Returns yellow when temp is between 18 to 24", function(){
+      thermostat.temp = 19;
+      expect(thermostat.displayColour()).toEqual("yellow");
+    });
 
-		it("Returns green when temp is below 18", function(){
-			thermostat.temp = 15;
-			expect(thermostat.displayColour()).toEqual("green");
-		});
-	});
+    it("Returns green when temp is below 18", function(){
+      thermostat.temp = 15;
+      expect(thermostat.displayColour()).toEqual("green");
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,42 +1,42 @@
 function Thermostat(){
-	this.temp = 20;
-	this.minTemp = 10;
-	this.isPowerSaving = true;
+  this.temp = 20;
+  this.minTemp = 10;
+  this.isPowerSaving = true;
 }
 
 Thermostat.prototype.up = function(){
-	if(this.temp===this._maxTemp()) throw "It's too hot already!";
-	this.temp++;
+  if(this.temp===this._maxTemp()) throw "It's too hot already!";
+  this.temp++;
 };
 
 Thermostat.prototype.down = function(){
-	if(this.temp===this.minTemp) throw "It's wayyyy too cold for that. Go back to Canada";
-	this.temp--;
+  if(this.temp===this.minTemp) throw "It's wayyyy too cold for that. Go back to Canada";
+  this.temp--;
 };
 
 Thermostat.prototype._maxTemp = function(){
-	return this.isPowerSaving ? 25 : 32;
+  return this.isPowerSaving ? 25 : 32;
 };
 
 Thermostat.prototype.togglePowerSave = function(){
-	if(this.isPowerSaving){
-		this.isPowerSaving=false;
-	} else {
-		if (this.temp > 25) this.temp = 25;
-		this.isPowerSaving=true;
-	}
+  if(this.isPowerSaving){
+    this.isPowerSaving=false;
+  } else {
+    if (this.temp > 25) this.temp = 25;
+    this.isPowerSaving=true;
+  }
 };
 
 Thermostat.prototype.resetTemp = function() {
-	this.temp = 20;
+  this.temp = 20;
 };
 
 Thermostat.prototype.displayColour = function() {
-	if(this.temp>=25){
-		return "red";
-	} else if (this.temp>=18) {
-		return "yellow";
-	} else {
-		return "green";
-	}
+  if(this.temp>=25){
+    return "red";
+  } else if (this.temp>=18) {
+    return "yellow";
+  } else {
+    return "green";
+  }
 };
