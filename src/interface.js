ce76345286e06b40c9a32df25d37a51053de7086
michaellennox <michaelctlennox@gmail.com>
2016-01-08 10:28:49 +0000

    Caches jQuery selectors

--- src/interface.js
+++ src/interface.js
@@ -2,39 +2,45 @@ var thermostat = new Thermostat();
 
 $(document).ready(function() {
   var $powerSave = $('#power-save');
+  var $up = $('#up');
+  var $down = $('#down');
+  var $reset = $('#reset');
+  var $temperature = $('#temperature');
+  var $display = $('#display');
+
   updateData();
 
-  $('#power-save').on('click', function() {
+  $powerSave.on('click', function() {
     thermostat.togglePowerSave();
     togglePowerSaveUI();
     updateData();
   });
 
-  $('#up').on('click', function() {
+  $up.on('click', function() {
     thermostat.up();
     updateData();
   });
 
-  $('#down').on('click', function() {
+  $down.on('click', function() {
     thermostat.down();
     updateData();
   });
 
-  $('#reset').on('click', function() {
+  $reset.on('click', function() {
     thermostat.resetTemp();
     updateData();
   });
-});
 
-function updateData() {
-  $('#temperature').text(thermostat.temp);
-  $('#display').css('background-color', thermostat.displayColour());
-}
-
-function togglePowerSaveUI() {
-  if (thermostat.isPowerSaving) {
-    $('#power-save').css('background-color', 'green');
-  } else {
-    $('#power-save').css('background-color', 'red');
+  function updateData() {
+    $temperature.text(thermostat.temp);
+    $display.css('background-color', thermostat.displayColour());
   }
-}
+
+  function togglePowerSaveUI() {
+    if (thermostat.isPowerSaving) {
+      $powerSave.css('background-color', 'green');
+    } else {
+      $powerSave.css('background-color', 'red');
+    }
+  }
+});
